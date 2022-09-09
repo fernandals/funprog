c4fd9e9dd7bf1dd298217e5d6048b26687229ef0
fernandals <lustosafk@gmail.com>
2022-09-09 18:27:20 -0300

    lesson 3

---
+++ fun/aulas/03/Primeiro.hs
@@ -0,0 +1,12 @@
+module Primeiro where
+
+-- Sejando o five != atribuição de variável
+five :: Integer
+five = 5
+
+addFive :: Integer -> Integer
+addFive x = x + five 
+--addFive x = undefined
+
+sempreSix :: Integer -> Integer
+sempreSix _ = 6
