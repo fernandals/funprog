3609ef65b0e56e0c384260c49d7af1dc2fa3ee73
fernandals <lustosafk@gmail.com>
2022-11-09 15:29:26 -0300

    class 13 corrections

--- fun/aulas/12/Talvez.hs
+++ fun/aulas/12/Talvez.hs
@@ -2,16 +2,19 @@ module Talvez where
 
 import Data.Maybe
 
+safeHead :: [a] -> Maybe a
+safeHead []    = Nothing
+safeHead (x:_) = Just x
+
 firstThat :: (a -> Bool) -> [a] -> Maybe a
 firstThat p []     = Nothing
 firstThat p (x:xs) = if p x then Just x
                      else firstThat p xs
+--firstThat p = safeHead . filter p
 
-isGoodFirstThat :: (a -> Bool) -> (a -> Bool) -> [a] -> Maybe a
-isGoodFirstThat p q xs
-  | isNothing firstValue = Nothing
-  | otherwise            = testValue p 
-  where firstValue = firstThat q xs
-        testValue p
-          | p (fromJust firstValue) = firstValue
-          | otherwise               = Nothing
+maybeize :: (a -> b) -> Maybe a -> Maybe b
+maybeize _ Nothing  = Nothing
+maybeize f (Just x) = Just (f x)
+
+isGoodFirstThat' :: (a -> Bool) -> (a -> Bool) -> [a] -> Maybe Bool
+isGoodFirstThat' good p = maybeize good . firstThat p
